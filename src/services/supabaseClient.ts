614dc1a4dfe32b3b0a7829b45882ba22997da0da
marianourra <marianourradev@gmail.com>
2026-01-22 12:11:36 -0300

    dale de una

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL='https://zvamxultqzecpxshxlld.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY='sb_publishable_eVBfgAQLiN2fxxKveo6QNw_cAg_K1e9';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zvamxultqzecpxshxlld.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_eVBfgAQLiN2fxxKveo6QNw_cAg_K1e9';
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
